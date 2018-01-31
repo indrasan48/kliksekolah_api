9e236d84550ac20ce4243ad5d69ad0a8a1c6702b
indrasan48 <indrasan.48@gmail.com>
2018-01-31 18:15:19 +0700

    ok fix index

--- controllers/index.php
+++ controllers/index.php
@@ -7,13 +7,7 @@ class index extends controller {
     }
 
     function index() {
-<<<<<<< HEAD
-		print_r( $_POST );
-		echo $_POST['method'];
-        //$this->view->render('index/index');
-=======
     	$this->view->render('index/index');
->>>>>>> cfc5e06873e82c001b6df1fb716099cac43f4d8d
     }
 
 }
--- views/index/index.php
+++ views/index/index.php
@@ -1,11 +1,20 @@
 <?php
-<<<<<<< HEAD
-
-    
-print_r ($_POST);
-echo $_POST['method'];
-
-
-=======
->>>>>>> cfc5e06873e82c001b6df1fb716099cac43f4d8d
+try{
+	glfn::_xml_http_request();
+	$arrayfields = $_POST;
+	$data_json = $arrayfields;
+	$url = URL.$_POST['method']."/".$_POST['function'];
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Requested-With: XMLHttpRequest'));
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	$return  = curl_exec($ch);	
+	curl_close($ch);	
+}catch(Exception $err){
+	$return = $err;
+}
+echo $return;
 ?>
