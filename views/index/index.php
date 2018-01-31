6ed0ebda297ce75c39048597dd774b9088ba94f6
indrasan48 <indrasan.48@gmail.com>
2018-01-31 18:36:36 +0700

    fix lg

--- views/index/index.php
+++ views/index/index.php
@@ -1,14 +1,12 @@
 <?php
 try{
 	glfn::_xml_http_request();
-	$arrayfields = $_POST;
-	$data_json = $arrayfields;
 	$url = URL.$_POST['method']."/".$_POST['function'];
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Requested-With: XMLHttpRequest'));
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 	$return  = curl_exec($ch);	
